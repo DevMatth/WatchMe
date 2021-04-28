fba223f92dadf98ec622b226b296101fbf1a1d4f
Matheus Leal <lealmirandamts@gmail.com>
2021-04-28 20:43:58 -0300

    complete aplication
    aplicação completa

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,24 @@
+import { MovieCard } from "./MovieCard";
+import { useMovies } from "../hooks/useMovies";
+
+import '../styles/content.scss'
+import { Header } from "./Header";
+
 export function Content() {
-  // Complete aqui
+
+  const { movies } = useMovies()
+
+  return (
+    <div className="container" >
+      <Header />
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+)
 }
